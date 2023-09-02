73c37a89915924d31d6eea3ef01fd99612a6973d
Prg-maker <df3296709@gmail.com>
2023-09-02 15:22:01 -0300

    add erro

--- src/Carro.java
+++ src/Carro.java
@@ -3,7 +3,7 @@ public class Carro {
   int ano, numPortas;
   boolean movimento;
   String marca, cor;
-  
+  //Illegal modifier for the constructor in type Carro; only public, protected & private are permitted
   public  Carro(String marca, String cor, int a, int n, boolean mov){
     this.marca=marca;
     this.cor=cor;
